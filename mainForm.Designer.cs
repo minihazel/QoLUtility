9341e7ae8db3914c47f35a7da6f4ff1eed86b2b6
minihazel <decyanidez@gmail.com>
2023-04-26 15:37:20 +0200

    Add project files.

---
+++ mainForm.Designer.cs
@@ -0,0 +1,92 @@
+﻿namespace QoLUtility
+{
+    partial class mainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.mainMenu = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(529, 210);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(91, 18);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Trader name";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Bahnschrift", 35F);
+            this.label2.Location = new System.Drawing.Point(522, 228);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(170, 57);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Prapor";
+            // 
+            // mainMenu
+            // 
+            this.mainMenu.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.mainMenu.Location = new System.Drawing.Point(12, 70);
+            this.mainMenu.Name = "mainMenu";
+            this.mainMenu.Size = new System.Drawing.Size(334, 445);
+            this.mainMenu.TabIndex = 1;
+            this.mainMenu.Paint += new System.Windows.Forms.PaintEventHandler(this.panel2_Paint);
+            // 
+            // mainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(28)))), ((int)(((byte)(28)))), ((int)(((byte)(28)))));
+            this.ClientSize = new System.Drawing.Size(1054, 664);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.mainMenu);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Bahnschrift Light", 11F);
+            this.ForeColor = System.Drawing.Color.LightGray;
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "mainForm";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.mainForm_Load);
+            this.Resize += new System.EventHandler(this.mainForm_Resize);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Panel mainMenu;
+    }
+}
+
---
+++ mainForm.cs
@@ -0,0 +1,216 @@
+﻿using QoLUtility.Properties;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QoLUtility
+{
+    public partial class mainForm : Form
+    {
+        public string currentDir = Environment.CurrentDirectory;
+
+        int default_item_height = 22; // item height
+        int default_item_loc_x = 1; // keep 1
+        int default_item_loc_y = 16; // item location startpoint
+        int default_item_spacer = 22; // spacer between item to item
+
+        public Color idleText = Color.LightGray;
+        public Color idleColor = Color.FromArgb(255, 28, 28, 28);
+        public Color hoverColor = Color.FromArgb(255, 32, 32, 32);
+        public Color selectColor = Color.FromArgb(255, 35, 35, 35);
+        public Color selectText = Color.DodgerBlue;
+        public Font idleFont = new Font("Bahnschrift Light", 11, FontStyle.Regular);
+
+        public string[] selectionMenu =
+        {
+            "Bots",
+            "Traders",
+            "Raids",
+            "Player",
+            "QoL"
+        };
+
+        public mainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void mainForm_Load(object sender, EventArgs e)
+        {
+            bool doDependenciesExist = checkDependencies();
+            if (doDependenciesExist)
+            {
+                MessageBox.Show("Works!");
+            }
+            else
+            {
+                MessageBox.Show("Not works");
+            }
+        }
+
+        public bool checkDependencies()
+        {
+            bool allExist = false;
+
+            /*
+            // - List all important directories & files to check;
+            // - - Check if they exist;
+            // - - - Verify
+            */
+
+            bool akiServerPath = Directory.Exists(Path.Combine(currentDir, "Aki.Server.exe"));
+            bool akiLauncherPath = Directory.Exists(Path.Combine(currentDir, "Aki.Launcher.exe"));
+            bool EFTData = Directory.Exists(Path.Combine(currentDir, "EscapeFromTarkov_Data"));
+
+            string[] akiDB_FullPath = { "Aki_Data", "Server", "database", "templates" };
+            string fullPath = string.Join("\\", akiDB_FullPath);
+            bool fullPathExists = Directory.Exists(Path.Combine(currentDir, fullPath));
+            bool akiDatabase = File.Exists(Path.Combine(akiDB_FullPath[3], "items.json"));
+
+            if (akiServerPath && akiLauncherPath && EFTData && fullPathExists && akiDatabase)
+            {
+                allExist = true;
+            }
+
+            return allExist;
+        }
+
+        private void clearUI(Panel control)
+        {
+            // server box
+            for (int i = control.Controls.Count - 1; i >= 0; i--)
+            {
+                Label selected = control.Controls[i] as Label;
+                if (selected != null)
+                {
+                    try
+                    {
+                        control.Controls.RemoveAt(i);
+                        selected.Dispose();
+                    }
+                    catch (Exception err)
+                    {
+                        Debug.WriteLine($"ERROR: {err.ToString()}");
+                        MessageBox.Show($"Oops! It seems like we received an error. If you're uncertain what it\'s about, please message the developer with a screenshot:\n\n{err.ToString()}", this.Text, MessageBoxButtons.OK);
+                    }
+                }
+            }
+        }
+
+        private void panel2_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics graphics = e.Graphics;
+            graphics.SmoothingMode = SmoothingMode.AntiAlias;
+            Rectangle rect = new Rectangle(0, 0, mainMenu.Width - 1, mainMenu.Height - 1);
+            int radius = 13;
+
+            GraphicsPath path = new GraphicsPath();
+            path.AddArc(rect.X, rect.Y, radius, radius, 180, 90);
+            path.AddArc(rect.Right - radius, rect.Y, radius, radius, 270, 90);
+            path.AddArc(rect.Right - radius, rect.Bottom - radius, radius, radius, 0, 90);
+            path.AddArc(rect.X, rect.Bottom - radius, radius, radius, 90, 90);
+            path.CloseAllFigures();
+
+            using (Pen pen = new Pen(Color.FromArgb(255, 100, 100, 100), 1))
+            {
+                graphics.DrawPath(pen, path);
+            }
+        }
+
+        public void listSystem(string[] arr)
+        {
+            clearUI(mainMenu);
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Label lbl = new Label();
+                lbl.Text = arr[i];
+                lbl.AutoSize = false;
+                lbl.Anchor = (AnchorStyles.Left | AnchorStyles.Top | AnchorStyles.Right);
+                lbl.TextAlign = ContentAlignment.MiddleLeft;
+                lbl.Size = new Size(mainMenu.Size.Width - 2, default_item_height);
+                lbl.Location = new Point(default_item_loc_x, default_item_loc_y + (i * default_item_spacer));
+                lbl.Font = idleFont;
+                lbl.BackColor = idleColor;
+                lbl.ForeColor = Color.Black;
+                lbl.Margin = new Padding(1, 1, 1, 1);
+                lbl.Cursor = Cursors.Hand;
+                lbl.MouseEnter += new EventHandler(lbl_MouseEnter);
+                lbl.MouseLeave += new EventHandler(lbl_MouseLeave);
+                lbl.MouseDown += new MouseEventHandler(lbl_MouseDown);
+                lbl.MouseUp += new MouseEventHandler(lbl_MouseUp);
+                lbl.TextAlign = ContentAlignment.MiddleLeft;
+                lbl.Visible = true;
+                mainMenu.Controls.Add(lbl);
+            }
+        }
+
+        private void lbl_MouseEnter(object sender, EventArgs e)
+        {
+            System.Windows.Forms.Label label = (System.Windows.Forms.Label)sender;
+            if (label.Text != "")
+            {
+                label.BackColor = hoverColor;
+            }
+        }
+
+        private void lbl_MouseLeave(object sender, EventArgs e)
+        {
+            System.Windows.Forms.Label label = (System.Windows.Forms.Label)sender;
+            if (label.Text != "")
+            {
+                label.BackColor = idleColor;
+            }
+        }
+
+        private void lbl_MouseDown(object sender, EventArgs e)
+        {
+            System.Windows.Forms.Label lbl = (System.Windows.Forms.Label)sender;
+            if (lbl.Text != "")
+            {
+                foreach (Control component in mainMenu.Controls)
+                {
+                    if (component.Text.Contains("> "))
+                    {
+                        //component.Text = component.Text.Remove(0, 2);
+                        component.Text = component.Text.Replace("> ", "");
+                    }
+
+                    if (component is Label && component.Text != lbl.Text)
+                    {
+                        component.BackColor = idleColor;
+                        component.ForeColor = idleText;
+                    }
+                }
+                lbl.BackColor = hoverColor;
+
+                string activeItem = lbl.Text;
+                activeItem = "> " + activeItem;
+                lbl.Text = activeItem;
+                lbl.ForeColor = selectText;
+
+            }
+        }
+
+        private void lbl_MouseUp(object sender, EventArgs e)
+        {
+            System.Windows.Forms.Label label = (System.Windows.Forms.Label)sender;
+            if (label.Text != "")
+            {
+            }
+        }
+
+        private void mainForm_Resize(object sender, EventArgs e)
+        {
+            mainMenu.Invalidate();
+        }
+    }
+}
